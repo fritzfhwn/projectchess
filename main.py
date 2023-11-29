704f3913ab3d6afe6e627d4bf196c1c5e0962ab8
fritzfhwn <fritz.thoendl@gmx.at>
2023-11-29 23:03:48 +0100

    adapting filtered userstats/userdetails so that only existing keys will generate the object; adding filtering method for date variables; adding a function that combines userstats and details based on an array-arg; games are extracted now as well, saved as json dump

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
+import json
+
 from chessdotcom import get_leaderboards, get_titled_players, get_country_players, Client, get_player_profile, \
-    get_player_stats
+    get_player_stats, get_player_games_by_month
 from datetime import datetime
 from isocodes import countries
-import requests
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 Client.request_config["headers"]["User-Agent"] = (
     "My Python Application. "
 )
-# response = requests.get(url, headers=headers)
-username = ""
-year = ""
-month = ""
 country_list = []
-game_details = f"https://api.chess.com/pub/player/{username}/games/{year}/{month}"
 
 for country in countries.items:
     country_list.append({"name": country["name"], "code": country["alpha_2"]})
+
 
 def check_key(object, key):
     if key in object.keys():
         return True
     else:
         return False
+
 
 def date_conversion(timestamp):
     return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
@@ -52,21 +47,76 @@ def bullet_leaderboard():
 
 def get_filtered_userdetails(user):
     userdata = get_player_profile(user).json["player"]
-    if check_key(userdata, "name"):
-        return {
-            "name": userdata["name"],
-            "username": user,
-            "country": userdata["country"][34::],
-            "is_streamer": userdata["is_streamer"]
-        }
+    dataset = ["name", "username", "country", "is_streamer"]
+    filtered_object = {}
+    for data in dataset:
+        if check_key(userdata, data):
+            filtered_object[data] = userdata[data]
+    return filtered_object
 
 
 def get_filtered_userstats(user):
     userstats = get_player_stats(user).json["stats"]
-    return {
-        "rapid": userstats["chess_rapid"],
-        "blitz": userstats["chess_blitz"],
-        "bullet": userstats["chess_bullet"],
-        "fide": userstats["fide"],
-        "puzzle_rush": userstats["puzzle_rush"]
-    }
+    modes = ["chess_rapid", "chess_blitz", "chess_bullet", "fide", "puzzle_rush"]
+    filtered_object = {}
+    for mode in modes:
+        if check_key(userstats, mode):
+            filtered_object[mode] = userstats[mode]
+    return filtered_object
+
+
+def filter_periods_from_start_and_end_date(start, end):
+    dates = []
+    if start <= end <= datetime.now().strftime("%Y-%m"):
+        counter_year = int(start[0:4:])
+        counter_month = int(start[5:7:])
+        while counter_year < int(end[0:4:]):
+            months_list = []
+            while counter_month <= 12:
+                months_list.append(str(counter_month))
+                counter_month += 1
+            dates.append({"year": counter_year,
+                          "months": months_list})
+            counter_year += 1
+            counter_month = 1
+        if counter_year == int(end[0:4:]):
+            months_list = []
+            while counter_month <= int(end[5:7:]):
+                months_list.append(str(counter_month))
+                counter_month += 1
+            dates.append({"year": counter_year,
+                          "months": months_list})
+        return dates
+
+
+def get_games_from_player_by_period(startdate, enddate, player_array):
+    dates = filter_periods_from_start_and_end_date(startdate, enddate)
+    games_list = []
+    for player in player_array:
+        for entry in dates:
+            for month in entry["months"]:
+                games = get_player_games_by_month(player, entry["year"], month).json["games"]
+                games_list.append({"player": player,
+                                   "year": entry["year"],
+                                   "month": month,
+                                   "games": games})
+        print(games_list)
+    json.dump(games_list, open("game_list.json", "w"), indent=6)
+
+
+def get_complete_profile(player_array):
+    output = []
+    for player in player_array:
+        stats_temp = get_filtered_userstats(player)
+        modes = ["chess_rapid", "chess_blitz", "chess_bullet"]
+        categories = ["last", "best"]
+        for mode in modes:
+            if check_key(stats_temp, mode):
+                for category in categories:
+                    if check_key(stats_temp[mode], category):
+                        stats_temp[mode][category]["date"] = date_conversion(stats_temp[mode][category]["date"])
+                output.append({"details": get_filtered_userdetails(player),
+                               "stats": stats_temp})
+    return json.dump(output, open("complete_player_profile_list.json", "w"), indent=6)
+
+
